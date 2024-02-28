6736c3b23e03525cbfa8b5699efe6c50b122c295
Danish <md236572@gmail.com>
2024-02-29 00:11:55 +0530

    backend commit

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,17 @@
 const express = require("express");
+const app = express();
+const cors = require("cors");
+const PORT = 3000
+app.use(cors());
+app.use(express.json());
 
+const rootRouter = require("./routes/index");
+
+app.use("/api/v1", rootRouter);
+
+
+
+app.listen(3000, () => {
+    console.log(`server is running on port ${PORT}`);
+});
 
---
+++ backend/routes/user.js
@@ -0,0 +1,126 @@
+const express = require('express');
+const router = express.Router();
+const zod = require("zod");
+const { User, Account } = require("../db/db"); // Corrected import
+
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../config");
+const { authMiddleware } = require('../middleware/authMiddleware');
+
+
+const signupBody = zod.object({
+    username: zod.string(),
+    firstName: zod.string(),
+    lastName: zod.string(),
+    password: zod.string()
+})
+
+router.post("/signup", async (req, res) => {
+    console.log(req.body);
+    const { success } = signupBody.safeParse(req.body);
+    if (!success) {
+        return res.status(411).json({
+            msg: "Incorrect Input"
+        });
+    }
+
+    const existingUser = await User.findOne({ username: req.body.username });
+    if (existingUser) {
+        return res.status(411).json({
+            msg: "User already exists"
+        });
+    }
+
+    try {
+        const user = await User.create({
+            username: req.body.username,
+            password: req.body.password,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName
+        });
+
+        const userId = user._id;
+
+        // Create an account and by default have default value in starting acc
+        await Account.create({
+            userId,
+            balance: 4000
+        });
+
+        const token = jwt.sign({ userId }, JWT_SECRET);
+
+        return res.send({
+            msg: "User created successfully",
+            token: token
+        });
+    } catch (error) {
+        console.error("Error creating user:", error);
+        return res.status(500).json({
+            msg: "Internal Server Error"
+        });
+    }
+});
+
+
+const signinBody = zod.object({
+    username : zod.string().email(),
+    password: zod.string()
+})
+
+router.post("/signin", async(req,res) => {
+    const { success } = signinBody.safeParse(req.body);
+
+    if(!success){
+        return res.status(411).json({
+            msg : "Incorrect input"
+        })
+    }
+
+    const user = await User.findOne({
+        username: req.body.username, // Correctly access username from request body
+        password: req.body.password
+    })
+
+    if (user) {
+        const token = jwt.sign({
+            userId: user._id
+        }, JWT_SECRET);
+  
+        res.json({
+            token: token
+        })
+        return;
+    }
+
+    res.status(411).json({
+        msg : "Error while login"
+    })
+
+})
+
+const updateBody = zod.object({
+    password: zod.string().optional(),
+    firstName: zod.string().optional(),
+    lastName: zod.string().optional()
+})
+
+router.put("/update", authMiddleware,async(req,res) =>{
+    const { success } = updateBody.safeParse(req.body);
+    if(!success){
+        return res.status(411).json({
+            msg : "Error while updating information"
+        })
+    }
+
+    await User.updateOne({
+        _id: req.userId
+    }, req.body)
+
+    res.json({
+        msg : "user updated successfully"
+    })
+
+})
+
+
+module.exports = router;
